97b83a7c2f914f888cdf40b9dd541fec76e30c44
sahrish <sahrishmanzoor2530@gmail.com>
2024-11-08 20:32:27 +0500

    Add files via upload

--- MedicinDB.java
+++ MedicinDB.java
@@ -1,28 +1,27 @@
 package com.example.unitconverter.ProviderInterface;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MedicinDB extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "medicin_database.db";
-    private static final int DATABASE_VERSION = 2; // Incremented version to handle schema changes
+    private static final String DATABASE_NAME = "medicinDB";
+    private static final int DATABASE_VERSION = 17;
 
-    // Medicine Donations table creation query
-    private static final String CREATE_TABLE_MEDICINE_DONATIONS =
-            "CREATE TABLE MedicineDonations (" +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "firstName TEXT, " +
-                    "lastName TEXT, " +
-                    "email TEXT, " +
-                    "streetAddress TEXT, " +
-                    "city TEXT, " +
-                    "postalCode TEXT, " +
-                    "phoneNumber TEXT, " +
-                    "equipmentType TEXT, " +
-                    "description TEXT, " +
-                    "comments TEXT);"; // Added comments column here for table creation
+    private static final String TABLE_NAME = "medicine_table";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_QUANTITY = "quantity";
+    private static final String COLUMN_MANUFACTURE_DATE = "manufacture_date";
+    private static final String COLUMN_EXPIRY_DATE = "expiry_date";
+    private static final String COLUMN_FORM = "form";
+    private static final String COLUMN_CONDITION = "condition";
+    private static final String COLUMN_LOCATION = "location";
+    private static final String COLUMN_LATITUDE = "latitude";
+    private static final String COLUMN_LONGITUDE = "longitude";
 
     public MedicinDB(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,34 +29,47 @@ public class MedicinDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_MEDICINE_DONATIONS);
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_QUANTITY + " TEXT, " +
+                COLUMN_MANUFACTURE_DATE + " TEXT, " +
+                COLUMN_EXPIRY_DATE + " TEXT, " +
+                COLUMN_FORM + " TEXT, " +
+                COLUMN_CONDITION + " TEXT, " +
+                COLUMN_LOCATION + " TEXT, " +
+                COLUMN_LATITUDE + " REAL, " +
+                COLUMN_LONGITUDE + " REAL)";
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < 2) {
-            // Add the Comments column in case it was missed in previous versions
-            db.execSQL("ALTER TABLE MedicineDonations ADD COLUMN Comments TEXT;");
-        }
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
-    // Insert method for saving donations
-    public boolean insertData(String firstName, String lastName, String email, String streetAddress,
-                              String city, String postalCode, String phoneNumber, String equipmentType,
-                              String description, String comments) {
+    public void addMedicine(String name, String quantity, String manufactureDate, String expiryDate,
+                            String form, String condition, String location, double latitude, double longitude) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "INSERT INTO MedicineDonations (firstName, lastName, email, streetAddress, city, postalCode, " +
-                "phoneNumber, equipmentType, description, comments) VALUES ('" + firstName + "', '" + lastName + "', '" +
-                email + "', '" + streetAddress + "', '" + city + "', '" + postalCode + "', '" + phoneNumber + "', '" +
-                equipmentType + "', '" + description + "', '" + comments + "');";
-        try {
-            db.execSQL(query);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        } finally {
-            db.close();
-        }
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_QUANTITY, quantity);
+        values.put(COLUMN_MANUFACTURE_DATE, manufactureDate);
+        values.put(COLUMN_EXPIRY_DATE, expiryDate);
+        values.put(COLUMN_FORM, form);
+        values.put(COLUMN_CONDITION, condition);
+        values.put(COLUMN_LOCATION, location);
+        values.put(COLUMN_LATITUDE, latitude);
+        values.put(COLUMN_LONGITUDE, longitude);
+
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public Cursor getAllMedicines() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_NAME, null, null, null, null, null, null);
     }
 }
